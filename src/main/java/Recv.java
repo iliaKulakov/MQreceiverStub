de2e2095e53600e9a0ad6b3424046de012376573
education20172017@gmail.com <Ltrf,hm_2018>
2019-03-17 16:44:19 +0300

    first commit  - project

---
+++ src/main/java/Recv.java
@@ -0,0 +1,27 @@
+import com.rabbitmq.client.*;
+
+public class Recv {
+
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] argv) throws Exception {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        ((ConnectionFactory) factory).setHost("localhost");
+        Connection connection = ((ConnectionFactory) factory).newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false,false,false,null);
+        System.out.println(" Waiting for messages. To exit please press CTR + C");
+
+        DeliverCallback deliverCallback = (String consumerTag, Delivery delivery) ->
+        {
+            String message = new String(delivery.getBody(),"UTF-8");
+            System.out.println("Received" + message + "'");
+        };
+
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag ->{});
+
+
+    }
+}
