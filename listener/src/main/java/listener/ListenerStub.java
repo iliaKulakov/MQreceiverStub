9bd115a8fbd41d9bec6c5275604c41ed74ec2aee
education20172017@gmail.com <Ltrf,hm_2018>
2019-04-14 21:23:19 +0300

    Added listener module

--- listener/src/main/java/listener/ListenerStub.java
+++ listener/src/main/java/listener/ListenerStub.java
@@ -1,13 +1,29 @@
 package listener;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
 
+@Configuration
 @SpringBootApplication
-public class ListenerStub {
+@EnableJms
+public class ListenerStub implements ApplicationRunner  {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(ListenerStub.class, args);
     }
+
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+//        sender.sendMessage();
+//        sender.sendSecondSystemMessage();
+//        consumer.listener();
+        System.out.println("Listener working now ");
+    }
+
 
 }
