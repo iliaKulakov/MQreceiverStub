d0f5c5a293153d0ef24938f25fd3c5d91d1776ec
education20172017@gmail.com <Ltrf,hm_2018>
2019-04-14 21:00:41 +0300

    Added listener module

---
+++ listener/src/main/java/listener/ListenerStub.java
@@ -0,0 +1,13 @@
+package listener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListenerStub {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ListenerStub.class, args);
+    }
+
+}
