6e318c79ddb7f0fd4d347eb22e0c8c835fe996cc
education20172017@gmail.com <Ltrf,hm_2018>
2019-04-24 23:50:25 +0300

    added module SenderWithoutSpring

--- sender.without.spring/src/main/java/SenderStubWithoutSpring.java
+++
@@ -1,8 +0,0 @@
-public class SenderStubWithoutSpring {
-
-    public static void main(String[] args){
-
-        System.out.println("Hello World");
-
-    }
-}
--- sender.without.spring/src/main/java/jms/SenderWithoutSpring.java
+++ sender.without.spring/src/main/java/jms/SenderWithoutSpring.java
@@ -1,13 +1,23 @@
 package jms;
 
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
 import javax.jms.*;
 import javax.naming.Context;
 import javax.naming.InitialContext;
+import java.util.Properties;
 
 public class SenderWithoutSpring  {
 
 
     public void sendMessage() throws Exception{
+
+        Properties props = new Properties();
+        props.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.activemq.jndi.ActiveMQInitialContextFactory");
+        props.setProperty(Context.PROVIDER_URL, "tcp://hostname:61616");
+        javax.naming.Context ctx = new InitialContext(props);
+
         Context context = new InitialContext();
         ConnectionFactory cf = (ConnectionFactory) context.lookup("SampleConnectionFactory");
         Destination queue = (Destination) context.lookup("mq.xml.out");
@@ -22,7 +32,7 @@ public class SenderWithoutSpring  {
 
     public static void main(String[] args) throws Exception{
 
-        System.out.println("Hello World");
+
 
         SenderWithoutSpring senderWithoutSpring = new SenderWithoutSpring();
         senderWithoutSpring.sendMessage();
