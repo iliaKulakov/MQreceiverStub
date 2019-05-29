2d4f858e8265337b67da3dfca0620f990f56fce5
education20172017@gmail.com <Ltrf,hm_2018>
2019-05-29 15:49:30 +0300

    Added ConfigHandling

--- listener/src/main/java/io/github/mqrecieverstub/listener/service/ConfigHandling.java
+++ listener/src/main/java/io/github/mqrecieverstub/listener/service/ConfigHandling.java
@@ -1,9 +1,21 @@
 package io.github.mqrecieverstub.listener.service;
 
+import io.github.mqrecieverstub.listener.repository.BankSystemInfoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ConfigHandling {
 
+    private BankSystemInfoRepository bankSystemInfoRepository;
+
+    @Autowired
+    public ConfigHandling(BankSystemInfoRepository bankSystemInfoRepository) {
+        this.bankSystemInfoRepository = bankSystemInfoRepository;
+    }
+
+    public void getConfigInfoFromDatabase(){
+
+    }
 
 }
