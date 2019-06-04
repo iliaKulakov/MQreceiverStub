c839d7e09606787c77f43e4e9e1d4af44e7b9f9a
education20172017@gmail.com <Ltrf,hm_2018>
2019-06-04 07:29:45 +0300

    Added DefaultConfigInfo in database when start a programm

--- listener/src/main/java/io/github/mqrecieverstub/listener/ListenerStub.java
+++ listener/src/main/java/io/github/mqrecieverstub/listener/ListenerStub.java
@@ -1,7 +1,10 @@
 package io.github.mqrecieverstub.listener;
 
 import io.github.mqrecieverstub.listener.jms.MessageProcessingListener;
+import io.github.mqrecieverstub.listener.service.ConfigProcessing;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jms.annotation.EnableJms;
@@ -10,13 +13,23 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication(scanBasePackages = "io.github.mqrecieverstub.listener")
 @EnableTransactionManagement
 @EnableJms
-public class ListenerStub {
+public class ListenerStub implements ApplicationRunner {
 
     @Autowired
     MessageProcessingListener messageProcessingListener;
 
+    @Autowired
+    ConfigProcessing configProcessing;
+
     public static void main(String[] args) {
         SpringApplication.run(ListenerStub.class, args);
     }
+
+    public void run(ApplicationArguments args) throws Exception{
+
+        configProcessing.putDefaultConfigInfoInDatabase();
+
+    }
+
 
 }
--- listener/src/main/java/io/github/mqrecieverstub/listener/service/ConfigProcessing.java
+++ listener/src/main/java/io/github/mqrecieverstub/listener/service/ConfigProcessing.java
@@ -21,17 +21,46 @@ public class ConfigProcessing {
     public ConfigProcessing(BankSystemInfoRepository bankSystemInfoRepository) {
         this.bankSystemInfoRepository = bankSystemInfoRepository;
     }
+//
+//    BankSystemsDomain getConfigInfoFromDb() {
+//        List<BankSystemsDomain> bankSystemsDomains = bankSystemInfoRepository.findAll();
+//        //TODO: Тут надо сразу определять тип
+//        Optional<Stream<BankSystemsDomain>> bankSystemDomainsStream = bankSystemsDomains.stream();
+//        Comparator<BankSystemsDomain> comparator = Comparator.comparing(BankSystemsDomain::getId);
+//
+//        //TODO: Тут возращается optional. Почему ты сразу возращаешь объект без проверки?
+//
+//        Optional<BankSystemsDomain> result = bankSystemDomainsStream.max(comparator).get();
+//        return result.isPresent() ? result.get() : defaultConfingVar;
+//
+//    }
 
-    BankSystemsDomain getConfigInfoFromDb() {
+    public BankSystemsDomain getConfigInfoFromDb() {
         List<BankSystemsDomain> bankSystemsDomains = bankSystemInfoRepository.findAll();
         //TODO: Тут надо сразу определять тип
-        Stream bankSystemDomainsStream = bankSystemsDomains.stream();
+        Stream<BankSystemsDomain> bankSystemDomainsStream = bankSystemsDomains.stream();
         Comparator<BankSystemsDomain> comparator = Comparator.comparing(BankSystemsDomain::getId);
 
         //TODO: Тут возращается optional. Почему ты сразу возращаешь объект без проверки?
 
-        Optional<BankSystemsDomain> result = (Optional<BankSystemsDomain>) bankSystemDomainsStream.max(comparator).get();
-        return result.isPresent() ? result.get() : defaultConfingVar;
+        Optional<BankSystemsDomain> result = bankSystemDomainsStream.max(comparator);
+
+        //Не стал удалять данное условие несмотря на то что у нас изначально есть дефолтный конфиг
+        //в бд, гипотетически чтобы исключить ситуации когда нам вернется null вместо него и записать
+        //дефолтную конфигурацию
+        return result.orElseGet(() -> defaultConfingVar);
 
     }
+
+    public void putDefaultConfigInfoInDatabase(){
+
+        List<BankSystemsDomain> bankSystemsDomains = bankSystemInfoRepository.findAll();
+
+        if (bankSystemsDomains.size()<=0){
+
+            bankSystemInfoRepository.save(defaultConfingVar);
+        }
+
+    }
+
 }
